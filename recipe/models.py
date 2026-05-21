e6dff255a3e343f33f3bc2cb78e05e162656522e
jcSbzeiu <aleksandrazavgorodnaa5@gmail.com>
2026-05-21 16:00:51 +0300

    initial commit with models and tests

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
 
+    def __iter__(self):
+        yield self.name
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    instructions = models.TextField()
+    ingredients = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
